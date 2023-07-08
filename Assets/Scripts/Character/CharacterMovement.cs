3fd8e1cf84db9c00faa2fc69622fbbd6ec0864c3
Altay Turan <69851770+altayturan@users.noreply.github.com>
2023-07-08 20:48:33 +0300

    feat: Add jump and change left right mechanic

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -13,6 +13,9 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private float torque = 30;
     [SerializeField] private float maxVelocityX = 10;
     [SerializeField] private float maxVelocityZ = 10;
+    [SerializeField] private float jumpForce=5;
+
+    private bool _isGrounded = true;
     
     private Vector3 _clampedVelocity;
 
@@ -26,12 +29,7 @@ public class CharacterMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            var targetX = Mathf.Clamp(transform.position.x - camera.transform.position.x, -5, 5);
-            var targetZ = Mathf.Clamp(transform.position.z - camera.transform.position.z, -5, 5);
-            
-            Debug.LogError($"X: {targetX}  Z: {targetZ}");
-            
-            var targetVector = new Vector3(targetX, 0,targetZ);
+            var targetVector = new Vector3(transform.position.x - camera.transform.position.x, 0,transform.position.z - camera.transform.position.z);
             rigidbody.AddForce(targetVector);
         }
         
@@ -47,16 +45,28 @@ public class CharacterMovement : MonoBehaviour
         
         if (Input.GetKey(KeyCode.A))
         {
-            rigidbody.AddTorque(new Vector3(0,-1,0) * torque,ForceMode.Force);
+            var targetVector = new Vector3(transform.position.x - camera.transform.position.x, 0,transform.position.z - camera.transform.position.z);
+            targetVector = Quaternion.AngleAxis(90, Vector3.up) * targetVector;
+            rigidbody.AddForce(-targetVector);
+            //rigidbody.AddTorque(new Vector3(0,-1,0) * torque,ForceMode.Force);
         }
         
         
         
         if (Input.GetKey(KeyCode.D))
         {
-            rigidbody.AddTorque(new Vector3(0,1,0) * torque,ForceMode.Force);
+             var targetVector = new Vector3(transform.position.x - camera.transform.position.x, 0,transform.position.z - camera.transform.position.z);
+             targetVector = Quaternion.AngleAxis(90, Vector3.up) * targetVector;
+            rigidbody.AddForce(targetVector);
+            //rigidbody.AddTorque(new Vector3(0,1,0) * torque,ForceMode.Force);
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!_isGrounded) return;
+            
+            rigidbody.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+        }
         
         
 
@@ -65,5 +75,15 @@ public class CharacterMovement : MonoBehaviour
         _clampedVelocity.z = Mathf.Clamp(rigidbody.velocity.z, -maxVelocityZ, maxVelocityZ);
 
         rigidbody.velocity = _clampedVelocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        _isGrounded = false;
     }
 }
