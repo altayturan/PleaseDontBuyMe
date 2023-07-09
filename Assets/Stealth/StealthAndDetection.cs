a82f894e4c59edde79d0c772d4fc61bb8c80751f
Ali Eren Aydın <aere612@gmail.com>
2023-07-09 19:06:22 +0300

    feat: Add shade animation and final stealth tweaks

--- Assets/Stealth/StealthAndDetection.cs
+++ Assets/Stealth/StealthAndDetection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -5,26 +6,56 @@ public class StealthAndDetection : MonoBehaviour
 {
     private Coroutine _detectionCountdownRoutine;
     [SerializeField] private float detectionPercentage;
+    [SerializeField] private float fadeInSpeed;//0.012f
+    [SerializeField] private float fadeOutSpeed;//0.0025f
+    [SerializeField] private GameController gameController;
+    [SerializeField] private bool alreadyDetected;
+    [SerializeField] private Renderer renderer;
+
     public void StartDetection()
     {
+        if (renderer.sharedMaterial.GetFloat("_Fade") == 0f || alreadyDetected) return;
+        alreadyDetected = true;
         _detectionCountdownRoutine = StartCoroutine(DetectionCountdownCoroutine());
+    }
+
+    private void Awake()
+    {
+        renderer.sharedMaterial.EnableKeyword("_Fade");
+    }
+
+    private void Update()
+    {
+        var currentFade = renderer.sharedMaterial.GetFloat("_Fade");
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            currentFade += fadeInSpeed;
+            if (currentFade > 1) currentFade = 1f;
+            renderer.sharedMaterial.SetFloat("_Fade", currentFade);
+        }
+        else
+        {
+            currentFade -= fadeOutSpeed;
+            if (currentFade < 0) currentFade = 0f;
+            renderer.sharedMaterial.SetFloat("_Fade", currentFade);
+        }
     }
 
     private IEnumerator DetectionCountdownCoroutine()
     {
         while (true)
         {
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) ||
-                Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            if (renderer.sharedMaterial.GetFloat("_Fade") == 0f)
             {
                 StopCoroutine(_detectionCountdownRoutine);
+                alreadyDetected = false;
                 break;
             }
-
             detectionPercentage += 5f;
             if (detectionPercentage >= 100)
             {
-                //lose
+                gameController.LoseGame();
             }
             yield return null;
         }
