3f26a3f0b31135739604fcc63ebd2d95b66060a5
Faruk <farukkayaduman@outlook.com>
2023-07-09 19:30:02 +0300

    feat: NPCs

--- Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
+++ Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -1,39 +1,98 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-namespace Pathfinding {
-	/// <summary>
-	/// Sets the destination of an AI to the position of a specified object.
-	/// This component should be attached to a GameObject together with a movement script such as AIPath, RichAI or AILerp.
-	/// This component will then make the AI move towards the <see cref="target"/> set on this component.
-	///
-	/// See: <see cref="Pathfinding.IAstarAI.destination"/>
-	///
-	/// [Open online documentation to see images]
-	/// </summary>
-	[UniqueComponent(tag = "ai.destination")]
-	[HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_a_i_destination_setter.php")]
-	public class AIDestinationSetter : VersionedMonoBehaviour {
-		/// <summary>The object that the AI should move to</summary>
-		public Transform target;
-		IAstarAI ai;
+namespace Pathfinding
+{
+    /// <summary>
+    /// Sets the destination of an AI to the position of a specified object.
+    /// This component should be attached to a GameObject together with a movement script such as AIPath, RichAI or AILerp.
+    /// This component will then make the AI move towards the <see cref="target"/> set on this component.
+    ///
+    /// See: <see cref="Pathfinding.IAstarAI.destination"/>
+    ///
+    /// [Open online documentation to see images]
+    /// </summary>
+    [UniqueComponent(tag = "ai.destination")]
+    [HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_a_i_destination_setter.php")]
+    public class AIDestinationSetter : VersionedMonoBehaviour
+    {
+        /// <summary>The object that the AI should move to</summary>
+        public Transform target;
 
-		void OnEnable () {
-			ai = GetComponent<IAstarAI>();
-			// Update the destination right before searching for a path as well.
-			// This is enough in theory, but this script will also update the destination every
-			// frame as the destination is used for debugging and may be used for other things by other
-			// scripts as well. So it makes sense that it is up to date every frame.
-			if (ai != null) ai.onSearchPath += Update;
-		}
+        [SerializeField] private bool isTargetRandomized;
+        [SerializeField] private List<Transform> targets;
+        private int currentTargetIndex;
 
-		void OnDisable () {
-			if (ai != null) ai.onSearchPath -= Update;
-		}
+        public static AIDestinationSetter Instance;
 
-		/// <summary>Updates the AI's destination every frame</summary>
-		void Update () {
-			if (target != null && ai != null) ai.destination = target.position;
-		}
-	}
+        IAstarAI ai;
+
+        void OnEnable()
+        {
+            ai = GetComponent<IAstarAI>();
+            // Update the destination right before searching for a path as well.
+            // This is enough in theory, but this script will also update the destination every
+            // frame as the destination is used for debugging and may be used for other things by other
+            // scripts as well. So it makes sense that it is up to date every frame.
+            if (ai != null) ai.onSearchPath += Update;
+        }
+
+        void OnDisable()
+        {
+            if (ai != null) ai.onSearchPath -= Update;
+        }
+
+        private void Start()
+        {
+            if(Instance == null)
+                Instance = this;
+        }
+
+        /// <summary>Updates the AI's destination every frame</summary>
+        void Update()
+        {
+            if (target != null && ai != null)
+            {
+                ai.destination = target.position;
+
+                float endReachedDistance = 10.0f;
+                if (Vector3.Distance(gameObject.transform.position, target.position) <= endReachedDistance)
+                {
+                    switch (isTargetRandomized)
+                    {
+                        case true:
+                            SetTargetToNextRandom();
+                            break;
+                        case false:
+                            SetTargetToNextInOrder();
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void SetTargetToNextInOrder()
+        {
+            currentTargetIndex++;
+            
+
+            target = targets[currentTargetIndex];
+        }
+
+        public void SetTargetToNextRandom()
+        {
+            int newTargetIndex = Random.Range(0, targets.Count);
+            if (newTargetIndex == currentTargetIndex)
+            {
+                newTargetIndex++;
+                if (newTargetIndex >= targets.Count)
+                    newTargetIndex = 0;
+            }
+            currentTargetIndex = newTargetIndex;
+            Debug.Log(newTargetIndex);
+
+            target = targets[currentTargetIndex];
+        }
+    }
 }
